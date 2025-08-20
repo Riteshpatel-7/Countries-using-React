8e9b84617f6c9e8cd64c27dec109fee71c24bc44
Ritesh Patel <rp390504@gmail.com>
2025-08-20 18:52:05 +0530

    Router fixes

--- index.jsx
+++ index.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Country from "./components/Country.jsx";
-import ShimmerForHome from "./components/ShimmerForHome.jsx";
 
 
 const router = createBrowserRouter([
